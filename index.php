81d38ae400c0fe1162a5a1c3b7a423e1886d530e
Ibrahim Rehman <ibrahimrehman0346@gmail.com>
2022-03-23 02:19:53 +0500

    Strings, Arrays and Associative Arrays

--- index.php
+++ index.php
@@ -18,49 +18,86 @@
 
       // Variables
 
-      $name = "Ibrahim Rehman";
-      $age = 20;
-      $height = 6.5;
-      $isMale = true;
-      $salary = null;
-      echo "Hello World"."<br>";
-      echo $name." ".$age." ".$isMale;
+      // $name = "Ibrahim Rehman";
+      // $age = 20;
+      // $height = 6.5;
+      // $isMale = true;
+      // $salary = null;
+      // echo "Hello World"."<br>";
+      // echo $name." ".$age." ".$isMale;
 
-      echo gettype($height);
-      var_dump($name);
+      // echo gettype($height);
+      // var_dump($name);
 
-      echo is_string($name);
-      echo isset($addr);
-
-
-      define('e', 2.72); // constants
-      echo e;
-
-      echo PHP_INT_MAX;
+      // echo is_string($name);
+      // echo isset($addr);
 
 
-      // Numbers
+      // define('e', 2.72); // constants
+      // echo e;
 
-      echo '<br><br>';
-      $a = 1;
+      // echo PHP_INT_MAX;
 
-      $b = 2.5;
-      $a++;
-      $a--;
-      ++$a;
-      --$a;
-      echo $a + 10;
 
-      echo is_float($a);
+      // // Numbers
 
-      $b = (int)$b;
-      echo gettype($b);
+      // echo '<br><br>';
+      // $a = 1;
 
-      // Some number functions are abs, pow, sqrt, min, max
+      // $b = 2.5;
+      // $a++;
+      // $a--;
+      // ++$a;
+      // --$a;
+      // echo $a + 10;
 
-      echo '<br>';
+      // echo is_float($a);
 
-      echo number_format(123456, 0, ',', ' ');
+      // $b = (int)$b;
+      // echo gettype($b);
+
+      // // Some number functions are abs, pow, sqrt, min, max
+
+      // echo '<br>';
+
+      // echo number_format(123456, 0, ',', ' ');
+
+
+      // strings
+
+      // $name = "Ibrahim Rehman";
+      // echo substr($name, 0, 5);
+
+      // $longText = "<h1>Hello</h1>
+      // World
+      // Hi ";
+
+      // echo nl2br(htmlentities($longText)); // preserves html code and line breaks
+
+      // arrays
+
+      // $fruits = ['Banana', "Orange"];
+      // $name = 'Hello World';
+      // $fruits[] = 'Apple';
+      // array_push($fruits, 'Watermelon');
+      // var_dump($fruits);
+
+      // echo implode(', ', $fruits);
+
+      // var_dump(explode(' ', $name));
+
+
+
+      // Associative Arrays i.e. objects in PHP 
+
+      // $personObj = ["firstKey" => 1, "secondKey" => 2];
+
+      // echo $personObj['firstKey'];
+
+
+      // $personObj['thirdKey'] ??= 3;
+
+      // var_dump($personObj);
     ?>
   </body>
 </html>
