1a606b87375e63c471128adbc110d928fe7d2612
Ibrahim Rehman <ibrahimrehman0346@gmail.com>
2022-03-25 19:36:14 +0500

    Cookies and Sessions in PHP

--- test.php
+++ test.php
@@ -6,22 +6,31 @@ $person = new Person();
 
 // Guzzle - PHP HTTP Client
 
-$client = new \GuzzleHttp\Client();
-$response = $client->request(
-    'GET',
-    'https://api.github.com/repos/guzzle/guzzle'
-);
+// $client = new \GuzzleHttp\Client();
+// $response = $client->request(
+//     'GET',
+//     'https://api.github.com/repos/guzzle/guzzle'
+// );
 
-echo $response->getStatusCode(); // 200
-echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
-echo $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
+// echo $response->getStatusCode(); // 200
+// echo $response->getHeaderLine('content-type'); // 'application/json; charset=utf8'
+// echo $response->getBody(); // '{"id": 1420053, "name": "guzzle", ...}'
 
-// Send an asynchronous request.
-$request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
-$promise = $client->sendAsync($request)->then(function ($response) {
-    echo 'I completed! ' . $response->getBody();
-});
+// // Send an asynchronous request.
+// $request = new \GuzzleHttp\Psr7\Request('GET', 'http://httpbin.org');
+// $promise = $client->sendAsync($request)->then(function ($response) {
+//     echo 'I completed! ' . $response->getBody();
+// });
 
-$promise->wait();
+// $promise->wait();
+
+// Cookies and Sessions
+
+session_start();
+echo session_id();
+
+session_destroy();
+
+setcookie('name', 'value', time() + 60);
 
 ?>
